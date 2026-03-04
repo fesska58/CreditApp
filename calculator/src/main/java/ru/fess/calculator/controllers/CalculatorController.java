79157cd4fbca23c867ff244da669d4627dcf2134
Alex <fess5800@mail.ru>
2026-03-04 11:45:59 +0300

    features/calculator добавил calculatorcontroller

---
+++ calculator/src/main/java/ru/fess/calculator/controllers/CalculatorController.java
@@ -0,0 +1,29 @@
+package ru.fess.calculator.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.fess.calculator.model.dto.LoanOfferDto;
+import ru.fess.calculator.model.dto.LoanStatementRequestDto;
+import ru.fess.calculator.service.CalculatorServiceIml;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/calculator")
+
+public class CalculatorController {
+    private final CalculatorServiceIml calculatorService;
+
+    public CalculatorController(CalculatorServiceIml calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @PostMapping("/offers")
+    public List<LoanOfferDto> calculateOffers(@Valid @RequestBody LoanStatementRequestDto requestDto){
+        return calculatorService.calculateOffers(requestDto);
+    }
+}
